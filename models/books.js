a4816804ff47941f9d0147d02eebff4d3cf7a1f1
David Gehtman <davidgehtman.91@gmail.com>
2020-03-12 14:46:32 -0400

    created the book schema

---
+++ models/books.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema ({
+    title: String,
+    authors: String,
+    description:String,
+    image: String,
+    link: String
+});
+
+const Book = mongoose.model ("Book", bookSchema);
+
+module.exports = Book;
+
+
