498c528f765c74449b80c621a5859dcbddd7bbba
David Gehtman <davidgehtman.91@gmail.com>
2020-03-14 10:52:21 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const apiRoutes = require("./controller/api-routes");
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,15 +12,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
-// Define API routes here
-app.use(apiRoutes);
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksreactsearch", { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`API Server now listening on PORT ${PORT}`);
 });
